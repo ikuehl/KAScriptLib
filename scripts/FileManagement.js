07e114194464df5ebe075f3b67384af3faa044a1
ikuehl <45126115+ikuehl@users.noreply.github.com>
2024-02-21 22:01:48 +0100

    initial

---
+++ scripts/FileManagement.js
@@ -0,0 +1,14 @@
+!INC Local Scripts.EAConstants-JavaScript
+!INC EAScriptLib.JavaScript-Logging
+
+var _Win32Import_FSREAD = 1;
+
+/*
+ * Script Name: FileManagement
+ * Author: Ingmar Kuehl
+ * Purpose: Reading, writing and parsing JSON files
+ * Date: 21.02.2024
+ */
+ 
+function readFile(filePath){
+}
