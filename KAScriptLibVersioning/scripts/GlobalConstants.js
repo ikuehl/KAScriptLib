e28eb1039658b545197d7894fd8fd9e8d1a0bcba
ikuehl <45126115+ikuehl@users.noreply.github.com>
2024-02-24 21:13:43 +0100

    moved files

---
+++ KAScriptLibVersioning/scripts/GlobalConstants.js
@@ -0,0 +1,13 @@
+!INC Local Scripts.EAConstants-JavaScript
+
+/* 
+ * Script Name: GlobalConstants
+ * Author: Ingmar Kuehl
+ * Purpose: Definitions globally used throughout versioning removal
+ * Date: 21.02.2024
+*/
+
+/**
+ * @brief Config file path
+ */
+const ConfigPath = "C:\\Entwicklung\\git\\versioning\\config.json";
