946198a659588741e4c4482710c4e749b98cd439
ikuehl <45126115+ikuehl@users.noreply.github.com>
2024-03-08 21:21:07 +0100

    enhanced doc.

--- KAScriptLibBase/scripts/FileManagement.js
+++ KAScriptLibBase/scripts/FileManagement.js
@@ -1,24 +1,28 @@
 /**
  * @file FileManagement
- * Reading, writing and parsing JSON files
+ * @description Reading, writing and parsing JSON files
  * @author Ingmar Kuehl, KUEHL AutomatisierungsTechnologie
  * @date 2024-02-21
+ * @license MIT
  */
  
 !INC Local Scripts.EAConstants-JavaScript
 !INC EAScriptLib.JavaScript-Logging
 
+/**
+ * @type Read
+ */
 var _Win32Import_FSREAD = 1;
 
 /**
- *  @description FileManagement
+ * @description FileManagement
  */
 class FileManagement {
 
 	/**
 	* @description Read a file and return the corresponding JSON object
-	* @param[in] {string} filePath The absolute path to a JSON file
-	* @return {JSONObject}
+	* @param {String} filePath The absolute path to a JSON file
+	* @return {Object} A JSON object
 	*/
 	readFile(filePath) {
 		// Open the provided file
@@ -38,9 +42,9 @@ class FileManagement {
 	}
 
 	/**
-	* @description Write an array to a JSON file. Pretach a generated timestamp.
-	* @param[in] {string} filePath Absolute file name path for output generation
-	* @param[in] {Map} argMap Map of ids and values to write. E.g. [{"startingPackage", EA.Package}, {"vcRes", Array}] 
+	* @brief Write an array to a JSON file. Pretach a generated timestamp.
+	* @param {String} filePath Absolute file name path for output generation
+	* @param {Object} argMap Map of ids and values to write. E.g. [{"startingPackage", EA.Package}, {"vcRes", Array}] 
 	*/
 	writeFile(filePath, argMap) {
 		var fsObject = new COMObject("Scripting.FileSystemObject");
